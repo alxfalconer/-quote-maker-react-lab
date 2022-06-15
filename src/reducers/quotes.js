cc72bec8016f001859fba56b456474c47cb575ed
Alexander <al.x.falconer@gmail.com>
2022-06-15 14:00:00 +0100

    1st commit

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,14 @@
 export default (state = [], action) => {
+  let index;
+   let quote;
+
+   switch (action.type) {
+
+     case 'ADD_QUOTE':
+       return state.concat(action.quote);
+
+     case 'REMOVE_QUOTE':
+       return state.filter(quote => quote.id !== action.quoteId);
   return state;
+}
 }
