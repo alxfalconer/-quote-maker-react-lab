bb836779ca5fe9956a5fa5c0906922bad05d5c96
Alexander <al.x.falconer@gmail.com>
2022-06-25 14:00:00 +0100

    container complete

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -31,4 +31,11 @@ class Quotes extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(Quotes);
+const mapStateToProps = state => {
+  return ({
+    quotes: state.quotes
+  })
+}
+
+export default connect(mapStateToProps, { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
+
